13b2e578ccbb0e4b379c4b95917ab7ab488389b7
kaito <k.takahashi.sys23@morijyobi.ac.jp>
2024-07-12 12:29:43 +0900

    CPUの手をランダムにする処理を実装した close #13, close # 14

--- src/Main.java
+++ src/Main.java
@@ -89,12 +89,13 @@ public class Main {
 
         }
 
+        //comの手を生成
 
+        com.createRandomHand();
 
         //以降にテストコードを追加して検証を行う
-
-
-
+        System.out.println("ランダムなジャンケンの手をCPUにセットする");
+        System.out.println(com.getHand().getHandName());
     }
 
 }
